67b0ba75af2942dfb2f6f09922092c98c6b0dac6
Ilmir <78535469+root0f@users.noreply.github.com>
2021-04-29 16:47:25 +0300

    fixed changes, continue later
    Create todolist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TodoList from "./Todo/TodoList";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    let todos = [
+        {id:1, completed:false, title: 'Практиковаться в йоге'},
+        {id:2, completed:false, title: 'Сделать пробежку'},
+        {id:3, completed:false, title: 'Купить еды'},
+        {id:4, completed:false, title: 'Купить дом'},
+]
 
+    function toggleTodo(id) {
+        todos = todos.map(todo => {
+            if(todo.id === id){
+                todo.completed = !todo.completed
+            }
+            return todo
+        })
+    }
+
+  return (
+      <div className="wrapper">
+          <h1>React</h1>
+
+        <TodoList todos={todos} onToggle={toggleTodo} />
+      </div>
+  )
+
+}
 export default App;
