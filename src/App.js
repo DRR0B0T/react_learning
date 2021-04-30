e85e9882a9fbab0ff15f6b9179994c8e4e73bdc1
Ilmir <78535469+root0f@users.noreply.github.com>
2021-04-30 12:52:54 +0300

    fixed changes

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import TodoList from "./Todo/TodoList";
 
 
 function App() {
-    let todos = [
+    const [todos, setTodos] = React.useState([
         {id:1, completed:false, title: 'Практиковаться в йоге'},
         {id:2, completed:false, title: 'Сделать пробежку'},
         {id:3, completed:false, title: 'Купить еды'},
         {id:4, completed:false, title: 'Купить дом'},
-]
+    ])
+
 
     function toggleTodo(id) {
-        todos = todos.map(todo => {
+        setTodos (
+            todos.map(todo => {
             if(todo.id === id){
                 todo.completed = !todo.completed
             }
             return todo
-        })
+        }))
     }
 
   return (
@@ -29,3 +31,4 @@ function App() {
 
 }
 export default App;
+
